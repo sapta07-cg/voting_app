89b2fc242e429c92bac0d3c86304316650714137
sapta07-cg <chakrabortysaptarshi03@gmail.com>
2025-08-02 20:20:10 +0530

    utils added

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+// const asyncHandler=(fn)=>async (req,res,next)=>{  // this function takig a function -- ()=>{async ()=>{}}
+//    try{
+
+//     await fn(req,res,next);
+
+//    }catch(err){
+//     res.status(err.code || 500).json({
+//         success:false,
+//         message: err.message
+//     })
+//    }
+
+// }
+
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((err)=>next(err))
+
+    }
+
+}
+
+export {asyncHandler}
+
