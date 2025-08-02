c2aff84be2be74b3fdbd4f21cf73d87bbfa9cd61
sapta07-cg <chakrabortysaptarshi03@gmail.com>
2025-08-02 12:02:42 +0530

    database connected

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const express=require('express');
-const app=express();
-const db=require('./db');
-require('dotenv').config();
+// const express=require('express');
+// const app=express();
+// const db=require('./db');
+// require('dotenv').config();
 
-const bodyParser=require('body-parser');
-app.use(bodyParser.json());
-
-
-const PORT= process.env.PORT || 3000
-
-//import the router file
-const userRoutes=require('./routes/userRoutes');
-const candidateRoutes=require('./routes/candidateRoutes');
+// const bodyParser=require('body-parser');
+// app.use(bodyParser.json());
 
 
-//use the router file
-app.use('/user',userRoutes);
-app.use('/candidate',candidateRoutes);
+// const PORT= process.env.PORT || 3000
+
+// //import the router file
+// const userRoutes=require('./routes/userRoutes');
+// const candidateRoutes=require('./routes/candidateRoutes');
 
 
-app.listen(PORT,()=>{
-    console.log("listening on port 3000");
-});
+// //use the router file
+// app.use('/user',userRoutes);
+// app.use('/candidate',candidateRoutes);
+
+
+// app.listen(PORT,()=>{
+//     console.log("listening on port 3000");
+// });
---
+++ src/index.js
@@ -0,0 +1,13 @@
+// require('dotenv').config();
+
+import dotenv from "dotenv"
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+
+console.log("app starting");
